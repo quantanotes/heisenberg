e4dcfff571436ecb56639cc31f328592f87c9dd4
Keane Moraes <keanejonathan3@gmail.com>
2023-04-29 08:52:29 -0400

    added implementation for index client

--- internal/index/client.go
+++ internal/index/client.go
@@ -1,8 +1,29 @@
 package index
 
+import (
+	"context"
+	"heisenberg/internal"
+	"storj.io/drpc/drpcconn"
+)
+
 type IndexClient struct {
+	client *pb.BaseClient
 }
 
 func NewIndexClient(addr string) (*IndexClient, error) {
-	return nil, nil
+	ic := &IndexClient{}
+	ic.base, err = pb.NewBaseClient(ctx, addr, internal.IndexService, NewDRPCIndexClient)
+	if err != nil {
+		return nil, err
+	}
+	return ic, nil
 }
+
+func (ic *IndexClient) Close() {
+	ic.base.Close()
+}
+
+func (ic *StoreClient) Ping(ctx context.Context) internal.Service {
+	return ic.base.Ping(ctx)
+}
+
