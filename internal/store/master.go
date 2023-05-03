c1fc5afc3108d7e767f141a3cad8fa017ae6c8c0
Moeed Dar <moeed.dar@Gmail.com>
2023-05-03 03:26:17 +0100

    Changed logging finished connect test

--- internal/store/master.go
+++ internal/store/master.go
@@ -19,27 +19,28 @@ type StoreMasterServer struct {
 
 func NewStoreMasterServer() (*StoreMasterServer, error) {
 	m := &StoreMasterServer{}
+	m.shard = *newShard()
 	return m, nil
 }
 
 func (m *StoreMasterServer) Run(ctx context.Context, addr string) error {
 	lis, server, err := internal.NewServer(ctx, addr, m)
 	if err != nil {
-		log.LogErrReturn("RunStoreMasterServer", err)
-
+		log.LogErrReturn("RunStoreMasterServer", err, m.identity())
 	}
 	m.server = server
 	m.lis = lis
-	log.Info(fmt.Sprintf("Starting master store server %s", addr), nil)
+	log.Info("starting master store server", m.identity())
 	return m.server.Serve(ctx, *m.lis)
 }
 
 func (m *StoreMasterServer) Close() {
-	log.Info(fmt.Sprintf("Closing master store server %s", (*m.lis).Addr().String()), nil)
+	log.Info("closing master store server", m.identity())
 	(*m.lis).Close()
 }
 
-func (s *StoreMasterServer) Ping(ctx context.Context, req *pb.Empty) (*pb.Pong, error) {
+func (m *StoreMasterServer) Ping(ctx context.Context, msg *pb.Empty) (*pb.Pong, error) {
+	log.Info("recieved ping", m.identity())
 	pong := &pb.Pong{
 		Id:      "0",
 		Master:  true,
@@ -49,49 +50,69 @@ func (s *StoreMasterServer) Ping(ctx context.Context, req *pb.Empty) (*pb.Pong, 
 	return pong, nil
 }
 
-func (s *StoreMasterServer) ConnectNode(ctx context.Context, req string) error {
-	c, err := NewStoreClient(ctx, req)
+func (m *StoreMasterServer) AddShard(ctx context.Context, msg *pb.Shard) (*pb.Empty, error) {
+	id := msg.Shard
+	log.Info(fmt.Sprintf("adding shard with id %s", id), m.identity())
+	err := m.shard.addShard(id)
 	if err != nil {
-		return log.LogErrReturn("ConnectNode", err)
+		return log.LogErrNilReturn[pb.Empty]("AddShard", err, m.identity())
+		cc
+	}
+	return nil, nil
+}
+
+func (m *StoreMasterServer) Connect(ctx context.Context, msg *pb.Connection) (*pb.Empty, error) {
+	addr := msg.Address
+	log.Info(fmt.Sprintf("connecting client to server at %s", addr), m.identity())
+	c, err := NewStoreClient(ctx, addr)
+	if err != nil {
+		return log.LogErrNilReturn[pb.Empty]("Connect", err, m.identity())
 	}
 
 	pong, err := c.Ping(ctx)
 	if err != nil {
-		return log.LogErrReturn("ConnectNode", err)
+		return log.LogErrNilReturn[pb.Empty]("Connect", err, m.identity())
 	}
 
 	switch pong.Service {
 	case uint32(internal.StoreService):
 		id := pong.Id
 		shard := *pong.Shard
-		s.shard.addReplica(c, id, shard)
+		err := m.shard.addReplica(c, id, shard)
+		if err != nil {
+			return log.LogErrNilReturn[pb.Empty]("Connect", err, m.identity())
+		}
 	default:
-		return log.LogErrReturn("ConnectNode", err)
+		return log.LogErrNilReturn[pb.Empty]("Connect", err, m.identity())
 	}
 
-	return nil
+	return nil, nil
 }
 
-func (s *StoreMasterServer) Get(ctx context.Context, req *pb.Key) (*pb.Pair, error) {
-	key := req.Key
-	collection := req.Collection
-	shard, err := s.shard.getShard(key)
+func (m *StoreMasterServer) Get(ctx context.Context, msg *pb.Key) (*pb.Pair, error) {
+	key := msg.Key
+	collection := msg.Collection
+	shard, err := m.shard.getShard(key)
 	if err != nil {
-		return log.LogErrNilReturn[pb.Pair]("Get", err)
+		return log.LogErrNilReturn[pb.Pair]("Get", err, m.identity())
 	}
 	// Select random replica of given shard
 	client, err := shard.choose()
 	if err != nil {
-		return log.LogErrNilReturn[pb.Pair]("Get", err)
+		return log.LogErrNilReturn[pb.Pair]("Get", err, m.identity())
 	}
 	res := client.Get(key, collection)
 	return res, nil
 }
 
-func (s *StoreMasterServer) Put(ctx context.Context, req *pb.Pair) (*pb.Empty, error) {
+func (m *StoreMasterServer) Put(ctx context.Context, msg *pb.Pair) (*pb.Empty, error) {
 	return nil, nil
 }
 
-func (s *StoreMasterServer) Delete(ctx context.Context, req *pb.Key) (*pb.Empty, error) {
+func (m *StoreMasterServer) Delete(ctx context.Context, msg *pb.Key) (*pb.Empty, error) {
 	return nil, nil
+}
+
+func (m *StoreMasterServer) identity() log.M {
+	return log.M{"host": (*m.lis).Addr().String()}
 }
