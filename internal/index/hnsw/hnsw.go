7b5e5b56c453723bfb0ef918e9b2adeeb6d1a46f
Moeed Dar <moeed.dar@Gmail.com>
2023-05-04 18:25:31 +0100

    Created go wrapper structs

--- internal/index/hnsw/hnsw.go
+++ internal/index/hnsw/hnsw.go
@@ -1,3 +1,4 @@
+//go:generate go tool cgo hnsw.go
 package hnsw
 
 //#cgo CFLAGS: -I./
@@ -5,9 +6,41 @@ package hnsw
 //#include <stdlib.h>
 //#include "hnsw_wrapper.h"
 import "C"
+import "unsafe"
 
-type hnswOptions struct{}
+type hnswOptions struct {
+	m  int
+	ef int
+}
 
 type hnsw struct {
-	index C.hnsw
+	index     C.HNSW
+	space     spaceType
+	dim       int
+	max       int
+	normalise bool
+	opts      hnswOptions
+}
+
+func newHnsw(space spaceType, dim int, max int, opts *hnswOptions) *hnsw {
+	return &hnsw{
+		index:     C.new_hnsw(),
+		normalise: space == index.cosine,
+		opts:      *opts,
+	}
+}
+
+func (h *hnsw) add(id int, vec []float32) error {
+	C.addPoint(h.index, (*C.float)(unsafe.Pointer(&vec[0])), C.ulong(id))
+	return nil
+}
+
+func (h *hnsw) delete(id int) error {
+	C.deletePoint(h.index, C.ulong(id))
+	return nil
+}
+
+func (h *hnsw) search(query []float32, k int) ([]int, error) {
+	C.search(h.index, (*C.float)(unsafe.Pointer(&query[0])), C.ulong(k))
+	return nil, nil
 }
--- internal/index/index.go
+++ internal/index/index.go
@@ -1,7 +1,15 @@
 package index
 
+type spaceType = int
+
+const (
+	ip     spaceType = 1
+	cosine           = 2
+	l2               = 3
+)
+
 type index interface {
-	init() error
+	init(space spaceType, dim int, max int) error
 	close() error
 	load() error
 	save() error
