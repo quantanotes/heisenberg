1a5f1baca798ca502d7c435e834b6b0804871b91
Moeed Dar <moeed.dar@Gmail.com>
2023-05-15 22:06:05 +0100

    Update main.go

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ import (
 
 func main() {
 	var path string = os.Getenv("HEISENBERG_PATH")
+	if path == "" {
+		path = "./.heisenberg/"
+	}
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		err = os.MkdirAll(path, 0755)
 		if err != nil {
